b6e29decbafefef27464f013a17314dc34736ec2
Shauryavrat Gaur <shauryavratxg19@ssd88j.org>
2018-02-09 10:29:40 -0800

    Initial commit

---
+++ app/src/main/java/com/shaurya/funfacts/FunFactsActivity.java
@@ -0,0 +1,70 @@
+package com.shaurya.funfacts;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class FunFactsActivity extends AppCompatActivity {
+    public static final String TAG = FunFactsActivity.class.getSimpleName();
+    private TextView factTextView;
+    private Button showFactButton;
+    private FactBook factBook = new FactBook();
+    private ColorWheel colorWheel = new ColorWheel();
+    private RelativeLayout relativeLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        //Assign views from Layout file
+        factTextView = (TextView) findViewById(R.id.factTextView);
+        showFactButton = (Button) findViewById(R.id.showFactButton);
+        relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                factTextView.setText(factBook.getFact());
+
+                int color = colorWheel.getColor();
+                relativeLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
+
+        Toast.makeText(this, "Yeah B O I we got an A C T I V I T A H", Toast.LENGTH_SHORT).show();
+        Log.d(TAG,"We're logging from the onCreate method!");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_fun_facts, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
